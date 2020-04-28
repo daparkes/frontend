3147aace9fc57d9f5d48425f3fdef12947315751
Dan Parkes <daniel.parkes@digital.hmrc.gov.uk>
2020-04-28 13:36:34 +0100

    Initial commit

---
+++ target/scala-2.13/routes/main/controllers/routes.java
@@ -0,0 +1,20 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/danielparkes/workspace/frontend2/conf/routes
+// @DATE:Tue Apr 28 13:35:23 BST 2020
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReverseHomeController HomeController = new controllers.ReverseHomeController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReverseHomeController HomeController = new controllers.javascript.ReverseHomeController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets(RoutesPrefix.byNamePrefix());
+  }
+
+}
---
+++ target/scala-2.13/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,15 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/danielparkes/workspace/frontend2/conf/routes
+// @DATE:Tue Apr 28 13:35:23 BST 2020
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
